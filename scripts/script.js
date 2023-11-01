66da2a06f84fb2aa9933f43c599b25f665e1b21f
Yahya Mouelhi <101068337+YahyaMouelhi@users.noreply.github.com>
2023-11-01 06:48:59 +0100

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,246 @@
+//          respect out privacy
+//  as for the hunters , indice 7 = uch_b1_s 
+//  and read more about us and our licences etc
+
+let pregnant = true;
+let money = 999999999;
+let kids = ["chouchou","ARBI","kAFON","Ahmed mo7sen","Klay BBJ"]
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+let weapon = "AK13";
+// part hahahahahahahahaha
+
+let BitsArray = [];
+let Tools = [];
+let Married = false;
+function selectionSort(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+    return arr;
+}
+
+// part hahahahahahahahaha
+// part hahahahahahahahaha
+
+const age = 18;
+const name = "Yahya Mouelhi"
+const currentDate = new Date();
+
+// #free_palestine ..
+
+function set_flag(left, right) {
+    let result = ["haha","you","are","stupid !"];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+BitsArray = ""
+const day = String(currentDate.getDate()).padStart(2, '0'); 
+const month = String(currentDate.getMonth() + 1).padStart(2, '0'); 
+Tools = "uploads"
+const year = currentDate.getFullYear();
+
+const idk = `${day}/${month}/${year}`;
+BitsArray += "4";
+let scores_grades = [10,78,32,19,34,56,18,2]
+let countries = true;
+var chorta = "true"
+
+let output = '';
+const Langs = ["html","css","python","javascript","perl"];
+Tools+=".html";
+
+function appendToOutput(value) {
+    output += value;
+    document.getElementById('output').value = output;
+}
+
+BitsArray += "_";
+function clearOutput() {
+    output = '';
+    "#flag";
+    document.getElementById('output').value = '';
+}
+
+let scores = [10,78,32,19,34,56,18,2]
+let country = true;
+var region = "tn"
+var region2 = "ps"
+var c = "I0cmNoNF9mMTRncyZfbTB1Y2hfYjFfczNo";
+BitsArray += "tn";
+BitsArray += "4";
+var wife,husband,family;
+
+// part hahahahahahahahaha
+function calculateResult() {
+    try {
+        let result = 0;
+        if (output == idk ){
+            (async () => {
+                const secretKey = crypto.getRandomValues(new Uint8Array(32)); 
+                const encryptedData = await encryptText(BitsArray, secretKey);
+                const decryptedText = await decryptText(encryptedData, secretKey);
+                alert("maybe 2 = "+ decryptedText)
+              })();
+              result = Tools;
+        }else{
+            result= eval(output);
+        }
+        document.getElementById('output').value = result;
+        output = result.toString();
+    } catch (error) {
+        document.getElementById('output').value = 'Error';
+        output = '';
+    }
+}
+
+// linux is good , but you must master basics
+function insertionSort(arr) {
+    const n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+            "part";
+            ".flag";
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+// part unavailable
+let b = "Y2g0bDRfdG40ajNtX3Q0bGdhX2";
+async function encryptText(text, secretKey) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+  
+    const cryptoKey = await crypto.subtle.importKey('raw', secretKey, 'AES-GCM', true, ['encrypt']);
+  
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+    const encryptedData = await crypto.subtle.encrypt({ name: 'AES-GCM', iv }, cryptoKey, data);
+  
+    return { iv, data: new Uint8Array(encryptedData) };
+  }
+  
+  async function decryptText(encryptedData, secretKey) {
+    const cryptoKey = await crypto.subtle.importKey('raw', secretKey, 'AES-GCM', true, ['decrypt']);
+  
+    const { iv, data } = encryptedData;
+    const decryptedData = await crypto.subtle.decrypt({ name: 'AES-GCM', iv }, cryptoKey, data);
+  
+    const decoder = new TextDecoder();
+    return decoder.decode(decryptedData);
+  }
+
+  a = "Q1lCRVJTSUNDQXtu"
+  function pizza(str) {
+    return str.replace(/[a-zA-Z]/g, function(c) {
+      var offset = c <= 'Z' ? 65 : 97;
+      return String.fromCharCode((c.charCodeAt(0) - offset + 13) % 26 + offset);
+    });
+  }
+
+
+  function bmw(str) {
+    return str.replace(/\s+/g, ' ').trim();
+}
+var e = "MV93dHc0bGwxX2g0Y2szZXJfazNzYTchfQ==";
+var d = "MV93dHc0bGwxX2g0Y2szZXJfazNzYTchfQ==";
+
+function insertionSort(arr) {
+    const n = arr.length;
+    for (let i = 1; i < n; i++) {
+        while (j >= 0 && arr[j] > key) {
+            ".flag";
+            break;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+let teta = c+d
+var f = true;
+function bugatti(){
+    document.querySelector(".ender").classList.toggle("hide");
+    document.querySelector(".end-creds").classList.toggle("hide");
+}
+    let alpha = a+b;
+  var guns = atob(alpha+teta);
+    function check(){
+        let inp = bmw(document.querySelector("#flag").value);
+        if (inp == guns){
+            document.querySelector(".end-creds").classList.toggle("hide");
+            document.querySelector(".ender").classList.toggle("hide");
+            document.title = "!!! CONGRATULATIONS !!!";
+        }else{
+            alert(false);
+        }
+    }
+
+  // part part part part part part , whoops
+  function selectionSort(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+    return arr;
+}
